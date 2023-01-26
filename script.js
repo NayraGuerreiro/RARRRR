84190303b8bb913d751ce345099281f979fedcf5
NayraGuerreiro <guerreironayra@gmail.com>
2023-01-25 21:44:00 -0300

    botao reiniciarã

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const header = document.getElementById("header");
 const areaJogo = document.getElementById("area-do-jogo");
 const personagem = document.getElementById("personagem");
 const obstaculo = document.getElementById("obstaculo");
+const gameOver = document.getElementById("game-over");
+const restartButton = document.getElementById("reiniciar");
 
 btnStar.addEventListener("click", () => {
   header.classList.add("hidden");
@@ -23,17 +25,64 @@ const loop = setInterval(() => {
     .getComputedStyle(personagem)
     .bottom.replace("px", "");
 
+  const gameOverScreen = () => {
+    gameOver.classList.add("modal");
+
+    // obstaculo.style.animation = "none";
+    // obstaculo.style.left = `${positionObstaculo}px`;
+
+    // personagem.style.animation = "none";
+    // personagem.style.bottom = `${positionPersonagem}px`;
+  };
+
+  window.getComputedStyle(obstaculo);
+
   if (
     positionObstaculo <= 180 &&
     positionObstaculo > 0 &&
     positionPersonagem < 80
   ) {
+    console.log("alo", positionObstaculo);
+    gameOverScreen();
+
     obstaculo.style.animation = "none";
     obstaculo.style.left = `${positionObstaculo}px`;
 
     personagem.style.animation = "none";
     personagem.style.bottom = `${positionPersonagem}px`;
+
+    clearInterval(loop);
   }
 }, 10);
+
+restartButton.addEventListener(
+  "click",
+  function (e) {
+    e.preventDefault;
+    restartAnimation();
+    gameOver.classList.remove("modal");
+    obstaculo.classList.remove("animation");
+    obstaculo.style = "animation";
+    obstaculo.offsetWidth = obstaculo.offsetWidth;
+    obstaculo.classList.add("animation");
+    personagem.classList.remove("jump");
+    personagem.style = "jump";
+    obstaculo.style.left = "";
+    personagem.style.bottom = "";
+  },
+  false
+);
+
+function restartAnimation(event) {
+  obstaculo.style.animationName = "none";
+  personagem.style.animationName = "none";
+  obstaculo.style.left = "none";
+  personagem.style.animation = "none";
+
+  requestAnimationFrame(() => {
+    obstaculo.style.animationName = "";
+    personagem.style.animationName = "";
+  });
+}
 
 document.addEventListener("keydown", jump);
